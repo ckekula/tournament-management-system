170e58a88b1506c69b46dc3e7e46e7a6f1f08a91
chamuditha123 <chamukeku@gmail.com>
2024-06-22 10:40:48 +0530

    Create Http interceptor

--- frontend/src/app/modules/comp/comp.module.ts
+++ frontend/src/app/modules/comp/comp.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CompRoutingModule } from './comp-routing.module';
 import { MainComponent } from './pages/main/main.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { CompCardComponent } from './components/comp-card/comp-card.component';
 
 @NgModule({
   declarations: [
     MainComponent,
-    MenuComponent
+    MenuComponent,
+    CompCardComponent
   ],
   imports: [
     CommonModule,
--- frontend/src/app/services/token/token.service.ts
+++ frontend/src/app/services/token/token.service.ts
@@ -14,25 +14,25 @@ export class TokenService {
     return localStorage.getItem('token') as string;
   }
 
-  isTokenValid() {
-    const token = this.token;
-    if (!token) {
-      return false;
-    }
-    // // decode the token
-    // const jwtHelper = new JwtHelperService();
-    // // check expiry date
-    // const isTokenExpired = jwtHelper.isTokenExpired(token);
-    // if (isTokenExpired) {
-    //   localStorage.clear();
-    //   return false;
-    // }
-    // return true;
-  }
+  // isTokenValid() {
+  //   const token = this.token;
+  //   if (!token) {
+  //     return false;
+  //   }
+  //   // decode the token
+  //   const jwtHelper = new JwtHelperService();
+  //   // check expiry date
+  //   const isTokenExpired = jwtHelper.isTokenExpired(token);
+  //   if (isTokenExpired) {
+  //     localStorage.clear();
+  //     return false;
+  //   }
+  //   return true;
+  // }
 
-  isTokenNotValid() {
-    return !this.isTokenValid();
-  }
+  // isTokenNotValid() {
+  //   return !this.isTokenValid();
+  // }
 
   get userRoles(): string[] {
     const token = this.token;

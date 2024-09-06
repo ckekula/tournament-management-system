8283044433d17f88dab5c8d79878f2d858a6c58a
chamuditha123 <chamukeku@gmail.com>
2024-09-06 14:46:21 +0530

    created user registration

---
+++ backend/src/main/java/com/tms/backend/auth/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.tms.backend.auth;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationResponse {
+    private String token;
+}

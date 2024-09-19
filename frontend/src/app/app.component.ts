b0f1a8de6148330ba45e7c851a1a27829e67d569
chamuditha123 <chamukeku@gmail.com>
2024-09-19 18:40:59 +0530

    Create Home page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {StyleClassModule} from 'primeng/styleclass';
+import {RippleModule} from 'primeng/ripple';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    StyleClassModule,
+    RippleModule,
+    ButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

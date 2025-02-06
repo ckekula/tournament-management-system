04e95baca7e6330afc421c304ed5763e79655c10
ckekula <chamukeku@gmail.com>
2025-02-06 09:10:06 +0530

    create profile pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,9 +7,12 @@ import { CompComponent } from './pages/view/comp/comp.component';
 import { TournamentComponent } from './pages/view/tournament/tournament.component';
 import { EventComponent } from './pages/view/event/event.component';
 import { ActivateAccountComponent } from './pages/auth/activate-account/activate-account.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
 import { EditCompComponent } from './pages/edit/edit-comp/edit-comp.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NewProfileComponent } from './pages/profile/new-profile/new-profile.component';
+import { NewOrgComponent } from './pages/profile/new-org/new-org.component';
+import { OrganizationComponent } from './pages/profile/organization/organization.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -17,10 +20,29 @@ export const routes: Routes = [
     { path: 'activate-account', component: ActivateAccountComponent },
     { path: 'register', component: RegisterComponent },
     { 
-        path: 'dashboard',
-        component: DashboardComponent,
+        path: 'profile',
+        component: ProfileComponent,
         canActivate: [AuthGuard]
     },
+    { 
+        path: 'new-profile',
+        component: NewProfileComponent,
+        canActivate: [AuthGuard]
+    },
+    { 
+        path: 'new-organization',
+        component: NewOrgComponent,
+        canActivate: [AuthGuard]
+    },
+    { 
+        path: 'organization/:id',
+        component: OrganizationComponent,
+        canActivate: [AuthGuard]
+    },
+    { path: 'competitions', component: CompetitionsComponent },
+    { path: 'comp/view/:id', component: CompComponent },
+    { path: 'tournament/view/:id', component: TournamentComponent },
+    { path: 'event/view/:id', component: EventComponent },
     { 
         path: 'comp/edit/:id',
         component: EditCompComponent,
@@ -36,8 +58,4 @@ export const routes: Routes = [
         component: EditCompComponent,
         canActivate: [AuthGuard]
     },
-    { path: 'competitions', component: CompetitionsComponent },
-    { path: 'comp/:id', component: CompComponent },
-    { path: 'tournament/:id', component: TournamentComponent },
-    { path: 'event/:id', component: EventComponent }
 ];
---
+++ frontend/src/app/pages/profile/new-org/new-org.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-org',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-org.component.html',
+  styleUrl: './new-org.component.scss'
+})
+export class NewOrgComponent {
+
+}

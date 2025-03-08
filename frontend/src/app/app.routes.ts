a2e0794d2c837480278b80cff7980357adb282aa
ckekula <chamukeku@gmail.com>
2025-03-08 10:34:21 +0530

    remove old files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/auth/register/register.component';
-import { CompComponent } from './pages/view/comp/comp.component';
-import { TournamentComponent } from './pages/view/tournament/tournament.component';
-import { EventComponent } from './pages/view/event/event.component';
 import { ActivateAccountComponent } from './pages/auth/activate-account/activate-account.component';
 import { AuthGuard } from './guards/auth.guard';
 import { EditCompComponent } from './pages/edit/edit-comp/edit-comp.component';
@@ -22,9 +19,6 @@ export const routes: Routes = [
         canActivate: [AuthGuard]
     },
     { path: 'org/:id', component: OrganizationComponent },
-    { path: 'comp/view/:id', component: CompComponent },
-    { path: 'tournament/view/:id', component: TournamentComponent },
-    { path: 'event/view/:id', component: EventComponent },
     { 
         path: 'comp/edit/:id',
         component: EditCompComponent,
